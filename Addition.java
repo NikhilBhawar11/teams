6e4266470c1e2375990ac48bd10b5bdd2754348e
AnantHon007 <ahon73716@gmail.com>
2025-02-27 22:49:42 -0800

    Update Addition.java

--- Addition.java
+++ Addition.java
@@ -1,7 +1,7 @@
 public class Addition {  
     public static void main(String[] args) {  
         int a = 5, b = 10;  
-        int sum = a + b;  
+        int sum = a  
         System.out.println("Sum: " + sum);  
     }  
 }
